9c6b5fc38a7564dc6b6ede8b4334fadcdd3431a0
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-06-27 21:30:50 +0700

    initial commit with profile card

---
+++ src/components/Skills.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+class Skills extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="skills">
+        <img
+          src="https://github.com/tandpfun/skill-icons/raw/main/icons/TypeScript.svg"
+          alt="TypeScript"
+        />
+        <img
+          src="https://github.com/tandpfun/skill-icons/raw/main/icons/React-Light.svg"
+          alt="React"
+        />
+        <img
+          src="https://github.com/tandpfun/skill-icons/raw/main/icons/JavaScript.svg"
+          alt="JavaScript"
+        />
+        <img
+          src="https://github.com/tandpfun/skill-icons/raw/main/icons/HTML.svg"
+          alt="HTML"
+        />
+        <img
+          src="https://github.com/tandpfun/skill-icons/raw/main/icons/CSS.svg"
+          alt="CSS"
+        />
+        <img
+          src="https://github.com/tandpfun/skill-icons/raw/main/icons/Python-Light.svg"
+          alt="Python"
+        />
+      </div>
+    );
+  }
+}
+
+export default Skills;
