884746f467895e45ef2df92ae6bd190d0ac2c1e9
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-06-29 22:28:27 +0700

    add somethings '~'

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,16 +1,35 @@
 import React from "react";
 
 class Avatar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clickCount: 0, isVisible: true };
+  }
   render() {
     return (
-      <div className="img-box">
-        <img
-          className="profile-img shadow-gray"
-          width={300}
-          src="/profile.jpeg"
-          alt="profile image"
-        />
-      </div>
+      <>
+        {this.state.isVisible && (
+          <div className="img-box">
+            <img
+              onClick={() => {
+                if (this.state.clickCount === 0) {
+                  alert("จะคลิกที่รูปภาพทำไม มันไม่มีอะไรหรอก");
+                } else if (this.state.clickCount === 1) {
+                  alert("ยังจะคลิกอีก! ก็บอกอยู่ว่ามันไม่มีอะไรยังไงเล่า");
+                } else if (this.state.clickCount === 2) {
+                  alert("ยัง ยัง ยังไม่หยุดอีก! ไม่ให้คลิกแล้ว");
+                  this.setState({ isVisible: false });
+                }
+                this.setState({ clickCount: this.state.clickCount + 1 });
+              }}
+              className="profile-img shadow-gray"
+              width={300}
+              src="/profile.jpeg"
+              alt="profile image"
+            />
+          </div>
+        )}
+      </>
     );
   }
 }
